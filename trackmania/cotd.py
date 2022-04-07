05831e2e0aea6fbab4fd0d6f9602f2cbe732b022
Deepesh Nimma <deepnimma@gmail.com>
2022-04-07 12:42:16 +0530

    more work on PlayerCOTD classes. get_page function for cotd data

--- trackmania/cotd.py
+++ trackmania/cotd.py
@@ -11,7 +11,7 @@ from trackmania.errors import TMIOException
 from .api import _APIClient
 from .config import Client
 from .constants import TMIO
-from .player import Player
+from .errors import TMIOException
 
 _log = logging.getLogger(__name__)
 
@@ -22,6 +22,22 @@ class BestCOTDStats:
 
     Parameters
     ----------
+    best_rank : int
+        The best rank achieved by the player.
+    best_rank_time : int
+        The time when `best_rank` was achieved.
+    best_rank_div_rank : int
+        The rank achieved in the division of the `best_rank`.
+    best_div : int
+        The best division of the player.
+    best_div_time : int
+        The time when `best_div` was achieved.
+    best_rank_in_div : int
+        The best rank the player achieved in any division.
+    best_rank_in_div_time : int
+        The time when `best_rank_in_div` was achieved.
+    best_rank_in_div_div : int
+        The division of the `best_rank_in_div`.
     """
 
     def __init__(
@@ -146,3 +162,175 @@ class PlayerCOTDStats:
             total_wins,
             win_streak,
         )
+
+
+class PlayerCOTDResults:
+    """
+    .. versionadded :: 0.3.0
+
+    Represents a Player's COTD Result.
+
+    Parameters
+    ----------
+    id : int
+        The ID of the COTD.
+    timestamp : :class:`datetime`
+        The timestamp of the COTD.
+    name : str
+        The name of the COTD
+    div : int
+        The division the player achieved
+    rank : int
+        The rank the player achieved
+    div_rank : int | None
+        The rank the player achieved in the division. If the player did not play in the division, this will be ``None``.
+    score : int | None
+        The score of the player. If the player did not play after qualifying for the COTD, this will be ``None``.
+    total_players : int
+        The total players that played this COTD.
+    """
+
+    def __init__(
+        self,
+        id: int,
+        timestamp: datetime,
+        name: str,
+        div: int,
+        rank: int,
+        div_rank: int | None,
+        score: int | None,
+        total_players: int,
+    ):
+        self.id = id
+        self.timestamp = timestamp
+        self.name = name
+        self.div = div
+        self.rank = rank
+        self.div_rank = div_rank
+        self.score = score
+        self.total_players = total_players
+
+    @classmethod
+    def _from_dict(cls, raw: Dict):
+        _log.debug("Creating a PlayerCOTDResults class from given dictionary")
+
+        id = raw["id"]
+        timestamp = datetime.strptime(raw["timestamp"], "%Y-%m-%dT%H:%M:%S+00:00")
+        name = raw["name"]
+        div = raw["div"]
+        rank = raw["rank"]
+        div_rank = raw["divrank"] if raw["divrank"] != 0 else None
+        score = raw["score"] if raw["score"] != 0 else None
+        total_players = raw["totalplayers"]
+
+        return cls(
+            id,
+            timestamp,
+            name,
+            div,
+            rank,
+            div_rank,
+            score,
+            total_players,
+        )
+
+
+class PlayerCOTD:
+    """
+    .. versionadded :: 0.3.0
+
+    The Player's COTD Data
+
+    Parameters
+    ----------
+    total : int
+        Total COTD's Played
+    recent_results : :class:`List[PlayerCOTDResults]`
+        Represents the recent COTD results the player has gotten
+    stats : :class:`PlayerCOTDStats`
+        Represents the Statistics of the Player's COTD career
+    player_id : str
+        The player's ID
+    """
+
+    def __init__(
+        self,
+        total: int,
+        recent_results: List[PlayerCOTDResults],
+        stats: PlayerCOTDStats,
+        player_id: str,
+    ):
+        self.total = total
+        self.recent_results = recent_results
+        self.stats = stats
+        self.player_id = player_id
+
+    @classmethod
+    def _from_dict(cls, page_data: Dict, player_id: str):
+        _log.debug("Creating a PlayerCOTD class from given dictionary")
+
+        total = page_data["total"]
+        stats = PlayerCOTDStats._from_dict(page_data["stats"])
+        player_id = player_id
+
+        recent_results = list()
+        for cotd in page_data["cotds"]:
+            recent_results.append(PlayerCOTDResults._from_dict(cotd))
+
+        return cls(
+            total,
+            recent_results,
+            stats,
+            player_id,
+        )
+
+    @staticmethod
+    async def get_page(player_id: str, page: int = 0):
+        """
+        .. versionadded :: 0.3.0
+
+        Gets the Player's COTD Stats of a particular page.
+
+        Parameters
+        ----------
+        player_id : str
+            The player's ID
+        page : int, optional
+            The page of the ID, by default 0
+        """
+        _log.debug(f"Getting COTD Stats for Player {player_id} and page {page}")
+
+        cache_client = redis.Redis(
+            host=Client.REDIS_HOST,
+            port=Client.REDIS_PORT,
+            db=Client.REDIS_DB,
+            password=Client.REDIS_PASSWORD,
+        )
+
+        with suppress(ConnectionRefusedError, redis.exceptions.ConnectionError):
+            if cache_client.exists(f"playercotd:{player_id}:{page}"):
+                # _log.debug(
+                #     f"Player {player_id}'s page {page} cotd results found in cache"
+                # )
+                return PlayerCOTD._from_dict(
+                    json.loads(
+                        cache_client.get(f"playercotd:{player_id}:{page}").decode(
+                            "utf-8"
+                        )
+                    ),
+                    player_id,
+                )
+
+        api_client = _APIClient()
+        page_data = await api_client.get(
+            TMIO.build([TMIO.TABS.PLAYER, player_id, TMIO.TABS.COTD, str(page)])
+        )
+        await api_client.close()
+
+        with suppress(KeyError, TypeError):
+            raise TMIOException(page_data["error"])
+        with suppress(ConnectionRefusedError, redis.exceptions.ConnectionError):
+            _log.debug(f"Caching Player {player_id} Page {page}")
+            cache_client.set(f"playercotd:{player_id}:{page}", json.dumps(page_data))
+
+        return PlayerCOTD._from_dict(page_data, player_id)
