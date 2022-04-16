bc9fdf910a397393947d2796b7cdd757906d4832
Deepesh Nimma <deepnimma@gmail.com>
2022-04-16 21:35:53 +0530

    trophy.py - incomplete

--- examples/trophy.py
+++ examples/trophy.py
@@ -1,1 +1,22 @@
-from trackmania import PlayerTrophies
+from typing import Dict, List
+
+from trackmania import Player, PlayerTrophies, trophy
+
+# There are no functions from `PlayerTrophies` that will give you an object of this class.
+# The ONLY way you can get a `PlayerTrophies` object is by calling `Player.get_player()`.
+
+
+async def run():
+    player_data = await Player.get_player("UID")
+    trophy_data = player_data.trophies
+
+    # Get Trophy Gain / Loss History of a Player
+    history = await trophy_data.history()
+
+    # Get the Total Trophy Score of a player
+    score = trophy_data.score()
+
+
+# Getting Top Players based on trophies
+async def top_trophies() -> List[trophy.TrophyLeaderboardPlayer]:
+    return NotImplemented
